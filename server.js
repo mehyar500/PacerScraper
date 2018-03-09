8236b42fa4d585da99804dd1d0987b3f72b0cf11
Mehyar <mehyar500@gmail.com>
2018-03-08 20:51:35 -0500

    node ready , results consoled ready, UI ready

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require('request');
+const cheerio = require('cheerio');
+const path = require("path");
+const fs = require("fs");
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+
+let pacerURL = "https://www.pacer.gov/announce.html";
+let linksTextArray = [];
+let linksArray = [];
+
+request.get(pacerURL, function(error, response, body){
+  let $ = cheerio.load(body);
+
+  if (error) {
+    console.log(error);
+  } else {
+    // console.log(body);
+    $(".linktxt").each(function(index, link) {
+      // console.log($(link).text().valueOf());
+      linksTextArray.push($(link).text().valueOf());
+
+      // console.log("https://www.pacer.gov"+$(link).attr("href").valueOf());
+      linksArray.push("https://www.pacer.gov"+$(link).attr("href").valueOf());
+    });
+
+    for (let i = 0; i < 15; i++) {
+      console.log(linksTextArray[i], linksArray[i]);
+    }
+
+  }
+
+});
+
+
+
+app.get("/", function(req, res) {
+
+});
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
